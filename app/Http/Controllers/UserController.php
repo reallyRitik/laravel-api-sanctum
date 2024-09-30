55ed3b7b0a64e2b51923125d98d562848dd06a5c
Ritik singh <106190674+reallyRitik@users.noreply.github.com>
2024-09-30 23:23:02 +0530

    compelete make Crud api using Sanctum

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function register(Request $request)
+    {
+        // Validate the request
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|confirmed|min:6',
+        ]);
+
+        // Create the user
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        // Generate token for the new user
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        // Return user and token in response
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+    }
+
+    public function logout()
+    {
+        // Delete the current token being used
+        auth()->user()->tokens()->delete();
+    
+        return response()->json([
+            'message' => 'Successfully logged out!',
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        // Validate the request
+        $request->validate([
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|confirmed|min:6',
+        ]);
+
+       $user= User::where('email', $request->email)->first();
+
+        if(!$user || !Hash::check($request->password, $user->password)){
+            return response([
+                'message' => 'The provided credentials are incorrect.'
+            ], 401);
+        }
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        // Return user and token in response
+        return response()->json([
+            'user' => $user,
+            'token' => $token,
+        ], 201);
+
+    }
+    
+    
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,13 +9,23 @@ use Illuminate\Support\Facades\Route;
 //     return 'my name is hritik singh';
 // });
 //PUBLIC ROUTE
-Route::get('/students', [StudentController::class, 'index']);
-Route::get('/students/{id}', [StudentController::class, 'show']);
-Route::post('/students', [StudentController::class, 'store']);
-Route::put('/students/{id}', [StudentController::class, 'update']);
-Route::delete('/students/{id}', [StudentController::class, 'destroy']);
-Route::get('/students/search/{city}', [StudentController::class, 'search']);
+// Route::get('/students', [StudentController::class, 'index']);
+// Route::get('/students/{id}', [StudentController::class, 'show']);
+// Route::post('/students', [StudentController::class, 'store']);
+// Route::put('/students/{id}', [StudentController::class, 'update']);
+// Route::delete('/students/{id}', [StudentController::class, 'destroy']);
+// Route::get('/students/search/{city}', [StudentController::class, 'search']);
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
+Route::post('/register', [UserController::class, 'register']);
+
+//Protected Route
+
+Route::middleware(['auth:sanctum'])->group (function(){
+    Route::get('/students', [StudentController::class, 'index']);
+    Route::get('/students/{id}', [StudentController::class, 'show']);
+    Route::post('/students', [StudentController::class, 'store']);
+    Route::put('/students/{id}', [StudentController::class, 'update']);
+    Route::delete('/students/{id}', [StudentController::class, 'destroy']);
+    Route::get('/students/search/{city}', [StudentController::class, 'search']);
+    Route::post('/logout', [UserController::class, 'logout']);
+});
